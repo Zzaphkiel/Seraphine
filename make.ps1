936526f54247ae6ef5fed4b98e10d1c839c38604
coolkiid <thoezitc@163.com>
2023-09-09 18:53:20 +0800

    [Github Actions] Add Github Actions
    Add Github Actions to automatically update Github Release according to
    latest push / merge requests on branch main

--- make.ps1
+++ make.ps1
@@ -11,10 +11,8 @@ rni -path .\dist\Seraphine\main.exe -newName Seraphine.exe
 cpi .\app -destination .\dist\Seraphine -recurse
 rm -r .\dist\Seraphine\app\common
 rm -r .\dist\Seraphine\app\components
-rm -r .\dist\Seraphine\app\config\*
 rm -r .\dist\Seraphine\app\lol
-rm -r .\dist\Seraphine\app\resource\game\*
 rm -r .\dist\Seraphine\app\resource\i18n\Seraphine.zh_CN.ts
 rm -r .\dist\Seraphine\app\view
-rar a $dest\Seraphine.rar .\dist\Seraphine\* -r -ep1
+7z a $dest\Seraphine.zip .\dist\Seraphine\* -r
 rm -r .\dist
