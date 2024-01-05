3fc5b9513560852815d05b68085d9b407f471980
Hpero4 <Hod7__@outlook.com>
2024-01-05 22:04:26 +0800

    BUG修复: 无法使用绝对路径运行 Seraphine #172

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from app.view.main_window import MainWindow
 
 
 if __name__ == '__main__':
-    os.environ['PATH'] = os.path.dirname(__file__) + os.pathsep + os.environ["PATH"]
+    os.chdir(os.path.dirname(__file__))
 
     if cfg.get(cfg.dpiScale) == "Auto":
         QApplication.setHighDpiScaleFactorRoundingPolicy(
