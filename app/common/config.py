b21d9dffa2df3f4741c30223e3bba42bd1dfbc40
Zzaphkiel <1278228162@qq.com>
2024-01-27 19:32:31 +0800

    更新版本号至 0.10.2

--- app/common/config.py
+++ app/common/config.py
@@ -102,7 +102,7 @@ class Config(QConfig):
 
 YEAR = 2023
 AUTHOR = "Zaphkiel"
-VERSION = "0.10.1"
+VERSION = "0.10.2"
 FEEDBACK_URL = "https://github.com/Zzaphkiel/Seraphine/issues"
 GITHUB_URL = "https://github.com/Zzaphkiel/Seraphine"
 
